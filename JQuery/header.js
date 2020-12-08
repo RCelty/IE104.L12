27303f489d9387a92a4c5b93a3b70c64af07ecb6
Ly Lu Gia Hung <18520798@gm.uit.edu.vn>
2020-12-08 17:27:37 +0700

    header_1,profile_1,image_add

--- JQuery/header.js
+++ JQuery/header.js
@@ -21,3 +21,12 @@ $(document).mouseup(function (e) {
         $('#Username').removeClass('c-blue');
     }
 });
+
+$(function(){
+    $(window).resize(function(){
+        var windowSize = $(window).innerWidth();
+        if(windowSize >= 900){
+            $('#navBar-icon').prop('checked', false);
+        }
+    });
+});
--- JQuery/menu_navBar_checkbox.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(window).resize(function(){
-        var windowSize = $(window).innerWidth();
-        if(windowSize >= 900){
-            $('#navBar-icon').prop('checked', false);
-        }
-    });
-});
