781ce24d96a1edc113c090a2e8e590f5559cfd6a
Ly Lu Gia Hung <18520798@gm.uit.edu.vn>
2020-12-13 22:44:29 +0700

    profile_6

--- components/profile-game/script.js
+++ components/profile-game/script.js
@@ -1,16 +1,17 @@
+position = 4;
+
 $(document).ready(function(){
-    $("#myDIV1").hide();
-    $("#myDIV2").hide();
+    $(".favourite div.col-lg-3").slice( position ).hide();
+    position = position + 1;
+    $(".myDIV2").hide();
     $("#show-all-favourite").prop('checked', false);
     $("#show-all-mygame").prop('checked', false);
 });
 
 function myFunction(tagname) {
-    var a = "#" + tagname;
-    console.log(a);
-    var x = document.getElementById(tagname);
-
-    if (x.style.display === "none") {
+    var a = "." + tagname;
+    console.log($(a).siblings(":visible"));
+    if (!$(a + ":visible").length) {
         $(a).show();
     } else {
         $(a).hide();
@@ -20,11 +21,19 @@ function myFunction(tagname) {
 $(".buttonupdown-hide1").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $(".favourite").offset().top
-    }, 1000);
+    }, 500);
 });
 
 $(".buttonupdown-hide2").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $(".mygame").offset().top
-    }, 1000);
+    }, 500);
 });
+
+function dislike(tagname){
+    var a = "#" + tagname;
+    $(a).delay(300).hide(600);
+    if (!$(".myDIV1:visible").length)
+        $(".favourite div.col-lg-3").slice(position - 1 , position).removeClass("myDIV1").delay(900).show(600);
+    position = position + 1;
+}
