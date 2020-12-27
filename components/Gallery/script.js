eaf35366df08bebc3e15475f6d8bbfefa52bdab9
Do Dinh Ngoc <18521154@gm.uit.edu.vn>
2020-12-27 11:23:48 +0700

    EditGallery_1

--- components/Gallery/script.js
+++ components/Gallery/script.js
@@ -26,3 +26,14 @@ prev.addEventListener("click", e => {
 
 let width = carousel.offsetWidth;
 window.addEventListener("resize", e => (width = carousel.offsetWidth));
+var modalEle = document.querySelector(".modal");
+var modalImage = document.querySelector(".modalImage");
+Array.from(document.querySelectorAll(".ImgThumbnail")).forEach(item => {
+   item.addEventListener("click", event => {
+      modalEle.style.display = "block";
+      modalImage.src = event.target.src;
+   });
+});
+document.querySelector(".close").addEventListener("click", () => {
+   modalEle.style.display = "none";
+});
